99b5eb67ce7cff93c6fa698ea74bfe141418f2e4
sistema-psico <sistema-psico@proton.me>
2025-12-11 11:59:49 -0300

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,11 @@
+export const PROJECT_STATUS = {
+  // --- INTERRUPTOR PRINCIPAL (KILL SWITCH) ---
+  // true = El software funciona correctamente
+  // false = Muestra la pantalla de "Servicio Suspendido"
+  isActive: true, 
+  
+  // Datos de contacto para la pantalla de bloqueo
+  providerName: "Eduardo Ricci", 
+  providerWhatsapp: "5493825625422", 
+  providerEmail: "edur900@gmail.com"
+};
