4794e9633fcdbcfe6c661048b3f6def79ddf1cd1
sistema-psico <sistema-psico@proton.me>
2025-12-11 14:22:34 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Usaremos las mismas variables de entorno para facilitar el despliegue
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
